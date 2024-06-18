7e34e9b4fecb716e8f8b107692e6ae4e9d84fed1
김지훈 <greenpig4b@gmail.com>
2024-06-19 07:45:05 +0900

    전문가 및 전체 CSS 수정

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -6,8 +6,12 @@ VALUES ('CLIENT', 'mimi@nate.com', '1234', '최미정', '0101112222', 'client1.j
        ('CLIENT', 'jin@nate.com', '1234', '김진호', '0101112222', 'client4.jpg', '1996-04-04', 'WOMAN', NOW(), NOW()),
        ('CLIENT', 'ji@nate.com', '1234', '서지호', '0101112222', 'client5.jpg', '1998-05-05', 'MAN', NOW(), NOW());
 
+-- 유저(상담가)
+INSERT INTO user_tb (user_role, email, password, name, phone, profile_image, birth, gender, created_at, updated_at)
+VALUES    ('EXPERT', 'expert01@nate.com', '1234', '강감찬', '01025151248', 'expert01.jpg', '1994-05-05', 'MAN', NOW(), NOW());
 -- comm_tb
 INSERT INTO comm_tb (user_id, created_at, content, title)
     VALUES (1, NOW(), '집에 가고 싶어서 고민 적습니다', '집에 가고 싶어요'),
              (2, NOW(), '저녁 메뉴를 못 정하겠어요. 추천받고 싶습니다', '배고파요'),
              (3, NOW(), '수업시간에 잠이 오면 어떻게 해야 할까요?','매일매일 졸아요');
+
