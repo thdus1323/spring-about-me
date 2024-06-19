aff0c85261189c9b29dc655ea8c5e21b10e2efec
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-06-19 15:33:41 +0900

    메인 커뮤니티 랜더링, 전무낙 파일 추가

--- src/main/java/com/example/aboutme/comm/CommRepository.java
+++ src/main/java/com/example/aboutme/comm/CommRepository.java
@@ -1,24 +1,27 @@
 package com.example.aboutme.comm;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CommRepository extends JpaRepository<Comm, Integer> {
-//    SELECT new com.example.aboutme.comm.CommResponse.ClientMainCommListDTO(c, c.user, r)
 
-//    @Query("""
-//            SELECT new com.example.aboutme.comm.CommResponse.ClientMainCommListDTO(
-//            c.id,
-//            c.title,
-//            c.content,
-//            c.category,
-//            c.user.profileImage,
-//            c.user.name,
-//            r.user.profileImage,
-//            r.user.name
-//            )
-//            FROM Comm c
-//            JOIN c.replies r
-//            WHERE r.user.userRole = com.example.aboutme.user.enums.UserRole.EXPERT
-//                    """)
-//    List<CommResponse.ClientMainCommListDTO> findCommsWithReply();
+    // 메인 커뮤니티 리스트
+    @Query("""
+            SELECT new com.example.aboutme.comm.CommResponse$ClientMainCommListDTO(
+            c.id, 
+            c.title, 
+            c.content, 
+            c.category, 
+            c.user.profileImage, 
+            c.user.name, 
+            r.user.profileImage, 
+            r.user.name
+            )
+            FROM Comm c
+            JOIN c.replies r
+            WHERE r.user.userRole = com.example.aboutme.user.enums.UserRole.EXPERT
+                    """)
+    List<CommResponse.ClientMainCommListDTO> findCommsWithReply();
 }
