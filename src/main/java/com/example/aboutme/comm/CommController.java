51c0c3334d066f48673f4dbc95383825fcbde57d
thdus1323 <thdus1323@naver.com>
2024-06-19 21:04:49 +0900

    상세보기 구현중

--- src/main/java/com/example/aboutme/comm/CommController.java
+++ src/main/java/com/example/aboutme/comm/CommController.java
@@ -1,9 +1,10 @@
 package com.example.aboutme.comm;
 
-import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,4 +15,16 @@ public class CommController {
     public String community() {
         return "comm/comm-write";
     }
+
+    @GetMapping("/comm-detail/{commId}")
+//    @GetMapping("/comm-detail")
+    public String detail(@PathVariable Integer commId, HttpServletRequest request) {
+        CommResponse.CommDetailDTO comm = commService.getCommDetail(commId);
+        request.setAttribute("comm", comm);
+        return "comm/comm-detail";
+    }
+
+    //게시판 상세보기
+
+
 }
--- src/main/java/com/example/aboutme/comm/CommService.java
+++ src/main/java/com/example/aboutme/comm/CommService.java
@@ -9,9 +9,16 @@ import java.util.List;
 @Service
 public class CommService {
     private final CommRepository commRepository;
+    private final CommNativeRepository commNativeRepository;
 
     public List<CommResponse.ClientMainCommListDTO> getMainComms() {
 
         return commRepository.findCommsWithReply();
+    }
+
+    //상품 상세보기
+    public CommResponse.CommDetailDTO getCommDetail(int commId) {
+        Comm comm = commNativeRepository.findById(commId);
+        return new CommResponse.CommDetailDTO(comm);
     }
 }
