1d9d3d8509e1e67c3f0720920e4de64b32247a7c
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-06-19 21:33:01 +0900

    어휴 왜 전문가가 안 되지?

--- src/main/java/com/example/aboutme/user/UserRepository.java
+++ src/main/java/com/example/aboutme/user/UserRepository.java
@@ -12,4 +12,18 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 
+//    @Query("""
+//            SELECT new com.example.aboutme.user.UserResponse$ExpertDTO(
+//            u.id,
+//            u.profileImage,
+//            u.name,
+//            u.expertTitle,
+//            v.id,
+//            v.voucherType
+//            )
+//            FROM User u
+//            LEFT JOIN Voucher v ON v.expert.id = u.id
+//            WHERE u.userRole = com.example.aboutme.user.enums.UserRole.EXPERT
+//            """)
+//    List<UserResponse.ExpertDTO> findExpertWithVoucher();
 }
