c50f10e150c1551816a9ffc0e337cae2e9ac2761
김지훈 <greenpig4b@gmail.com>
2024-06-21 08:40:19 +0900

    상담사찾기 검색기능(날짜)

---
+++ src/main/java/com/example/aboutme/user/UserRestController.java
@@ -0,0 +1,32 @@
+package com.example.aboutme.user;
+
+import com.example.aboutme._core.utils.ApiUtil;
+import com.example.aboutme.user.UserResponseDTO.expertFindDTO.FindWrapperRecord;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RequiredArgsConstructor
+@RestController
+public class UserRestController {
+    private final UserService userService;
+
+
+    @GetMapping("/users/times")
+    public ResponseEntity<?> getUsersTimes(@RequestParam String date) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
+
+        FindWrapperRecord findWrapperRecord = userService.getExpertFindBySearch(dateTime);
+        System.out.println(findWrapperRecord);
+
+        return ResponseEntity.ok(new ApiUtil(findWrapperRecord));
+    }
+
+
+}
