3b6bcec48d7b8aabea864ac77a601d3d0a005849
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-06-20 15:21:44 +0900

    더미 수정

--- src/main/java/com/example/aboutme/review/Review.java
+++ src/main/java/com/example/aboutme/review/Review.java
@@ -3,16 +3,11 @@ package com.example.aboutme.review;
 import com.example.aboutme.counsel.Counsel;
 import com.example.aboutme.user.User;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -31,7 +26,7 @@ public class Review {
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "counsel_id", nullable = false)
-    private Counsel counsel;  // 외래 키로 상담 정보를 참조
+    private Counsel counsel; // 외래 키로 상담 정보를 참조
 
     @Column(nullable = false)
     private String content;
