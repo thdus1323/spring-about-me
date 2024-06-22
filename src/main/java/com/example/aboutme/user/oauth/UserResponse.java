eb357637183bdfab09412767b005c0ebceca4982
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-06-22 15:08:40 +0900

    네이버 로그인

--- src/main/java/com/example/aboutme/user/oauth/UserResponse.java
+++ src/main/java/com/example/aboutme/user/oauth/UserResponse.java
@@ -41,4 +41,42 @@ public class UserResponse {
             }
         }
     }
+
+    @Data
+    public static class NaverTokenDTO {
+        @JsonProperty("access_token")
+        private String accessToken;
+
+        @JsonProperty("token_type")
+        private String tokenType;
+
+        @JsonProperty("refresh_token")
+        private String refreshToken;
+
+        @JsonProperty("expires_in")
+        private Integer expiresIn;
+
+        @JsonProperty("scope")
+        private String scope;
+    }
+
+    @Data
+    public static class NaverUserDTO {
+        private Long id;
+        private String resultcode;
+        private String message;
+        @JsonProperty("response")
+        private Response response;
+
+        @Data
+        public static class Response {
+            private String email;
+            private Profile profile;
+
+            @Data
+            public static class Profile {
+                private String nickname;
+            }
+        }
+    }
 }
