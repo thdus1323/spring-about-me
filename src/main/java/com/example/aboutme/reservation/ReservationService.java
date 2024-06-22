e6b90dd7e068a47f97215fd9f263e6a44b967fe1
kjh5848 <kjh5848@gmail.com>
2024-06-22 12:58:43 +0900

    예약, 스케줄 테이블 수정

--- src/main/java/com/example/aboutme/reservation/ReservationService.java
+++ src/main/java/com/example/aboutme/reservation/ReservationService.java
@@ -1,26 +1,16 @@
 package com.example.aboutme.reservation;
 
-import com.example.aboutme.reservation.resrvationResponse.ReservationListDTO;
-import com.example.aboutme.schedule.Schedule;
 import com.example.aboutme.schedule.ScheduleRepository;
-import com.example.aboutme.schedule.ScheduleResponse.AvailableTimeRecord;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 @Service
 public class ReservationService {
     private final ReservationRepository reservationRepository;
-    private final ScheduleRepository scheduleRepository;
 
 
 
@@ -36,19 +26,6 @@ public class ReservationService {
 
     }
 
-    //전문가 스케줄 불러오기
-    public List<ReservationListDTO> getExpertSchedules(Integer expertId,Integer voucherId) {
-        return scheduleRepository.findByExpertId(expertId).stream()
-                .map(schedule -> new ReservationListDTO(
-                        schedule.getExpert().getId(),
-                        schedule.getStartDate().toString(),
-                        schedule.getEndDate().toString(),
-                        schedule.getStartHour().toString(),
-                        schedule.getEndHour().toString(),
-                        schedule.getRestType().toString(),
-                        schedule.getDayOfWeek().toString(),
-                        schedule.getSpecificDate() != null ? schedule.getSpecificDate().toString() : null
-                ))
-                .collect(Collectors.toList());
-    }
 }
+
+
