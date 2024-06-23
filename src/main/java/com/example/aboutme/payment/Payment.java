0557b64f5e1c120c792cceb000999b5188638f47
kjh5848 <kjh5848@gmail.com>
2024-06-23 23:03:41 +0900

    결제시 컴플리트 확인완료 예약 테이블도 확정 대기로 변경완료

--- src/main/java/com/example/aboutme/payment/Payment.java
+++ src/main/java/com/example/aboutme/payment/Payment.java
@@ -43,6 +43,7 @@ public class Payment {
 
     private String impUid; // 아임포트 거래 고유 ID
 
+    @Column(nullable = false, unique = true)
     private String merchantUid; // 상점 거래 고유 ID
 
     @Builder
