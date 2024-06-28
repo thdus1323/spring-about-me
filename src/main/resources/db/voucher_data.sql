901f942c6959e8251282fbec16b7b3526ba0ad53
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-06-28 15:43:42 +0900

    바우처 더미 수정

--- src/main/resources/db/voucher_data.sql
+++ src/main/resources/db/voucher_data.sql
@@ -5,46 +5,58 @@ VALUES
     -- Expert 1 has 3 voucher types
     ('TEXT_THERAPY', 21, 15000, 1, 50, '/images/chat.png', NOW(), NOW()),
     ('TEXT_THERAPY', 21, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
-    ('VOICE_THERAPY', 21, 25000, 1, 30, '/images/call.png', NOW(), NOW()),
-    ('VIDEO_THERAPY', 21, 15000, 1, 40, '/images/video.png', NOW(), NOW());
+    ('VOICE_THERAPY', 21, 25000, 3, 30, '/images/call.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 21, 30000, 2, 40, '/images/video.png', NOW(), NOW()),
 
---     -- Expert 1 has 3 voucher types
---     (1, 'TEXT_THERAPY', 21, 10000, 1, 60, '/images/chat.png', NOW(), NOW()),
---     (2, 'VOICE_THERAPY', 21, 15000, 1, 30, '/images/call.png',NOW(), NOW()),
---     (3, 'VIDEO_THERAPY', 21, 20000, 1, 45, '/images/video.png', NOW(), NOW()),
---
---     -- Expert 2 has 2 voucher types
---     (4, 'TEXT_THERAPY', 22, 10000, 1, 60, '/images/chat.png',  NOW(), NOW()),
---     (5, 'VOICE_THERAPY', 22, 15000, 1, 30, '/images/call.png',  NOW(), NOW()),
---
---     -- Expert 3 has 1 voucher type
---     (6, 'VIDEO_THERAPY', 23, 20000, 1, 45, '/images/video.png', NOW(), NOW()),
---
---     -- Expert 4 has 2 voucher types
---     (7, 'TEXT_THERAPY', 24, 10000, 1, 60, '/images/chat.png',  NOW(), NOW()),
---     (8, 'VIDEO_THERAPY', 24, 20000, 1, 45, '/images/video.png',  NOW(), NOW()),
---
---     -- Expert 5 has 3 voucher types
---     (9, 'TEXT_THERAPY', 25, 10000, 1, 60, '/images/chat.png',  NOW(), NOW()),
---     (10, 'VOICE_THERAPY', 25, 15000, 1, 30, '/images/call.png',  NOW(), NOW()),
---     (11, 'VIDEO_THERAPY', 25, 20000, 1, 45, '/images/video.png',  NOW(), NOW()),
---
---     -- Expert 6 has 1 voucher type
---     (12, 'TEXT_THERAPY', 26, 10000, 1, 60, '/images/chat.png',  NOW(), NOW()),
---
---     -- Expert 7 has 2 voucher types
---     (13, 'VOICE_THERAPY', 27, 15000, 1, 30, '/images/call.png',  NOW(), NOW()),
---     (14, 'VIDEO_THERAPY', 27, 20000, 1, 45, '/images/video.png',  NOW(), NOW()),
---
---     -- Expert 8 has 3 voucher types
---     (15, 'TEXT_THERAPY', 28, 10000, 1, 60, '/images/chat.png',  NOW(), NOW()),
---     (16, 'VOICE_THERAPY', 28, 15000, 1, 30, '/images/call.png',  NOW(), NOW()),
---     (17, 'VIDEO_THERAPY', 28, 20000, 1, 45, '/images/video.png',  NOW(), NOW()),
---
---     -- Expert 9 has 2 voucher types
---     (18, 'TEXT_THERAPY', 29, 10000, 1, 60, '/images/chat.png', NOW(), NOW()),
---     (19, 'VOICE_THERAPY', 29, 15000, 1, 30, '/images/call.png',  NOW(), NOW()),
---
---     -- Expert 10 has 1 voucher type
---     (20, 'VIDEO_THERAPY', 30, 20000, 1, 45, '/images/video.png',NOW(), NOW());
---
+    -- Expert 22
+    ('TEXT_THERAPY', 22, 15000, 1, 50, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 22, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
+    ('VOICE_THERAPY', 22, 25000, 3, 30, '/images/call.png', NOW(), NOW()),
+
+    -- Expert 23
+    ('TEXT_THERAPY', 23, 15000, 2, 50, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 23, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 23, 15000, 2, 40, '/images/video.png', NOW(), NOW()),
+
+    -- Expert 24
+    ('VOICE_THERAPY', 24, 25000, 3, 30, '/images/call.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 24, 15000, 4, 40, '/images/video.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 24, 20000, 2, 45, '/images/video.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 24, 30000, 1, 60, '/images/video.png', NOW(), NOW()),
+
+    -- Expert 25
+    ('TEXT_THERAPY', 25, 15000, 1, 50, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 25, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
+    ('VOICE_THERAPY', 25, 25000, 1, 30, '/images/call.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 25, 40000, 1, 40, '/images/video.png', NOW(), NOW()),
+
+    -- Expert 26
+    ('TEXT_THERAPY', 26, 20000, 1, 50, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 26, 20000, 4, 50, '/images/chat.png', NOW(), NOW()),
+    ('VOICE_THERAPY', 26, 30000, 3, 30, '/images/call.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 26, 45000, 5, 40, '/images/video.png', NOW(), NOW()),
+
+    -- Expert 27
+    ('TEXT_THERAPY', 27, 15000, 1, 50, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 27, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
+    ('VOICE_THERAPY', 27, 25000, 1, 30, '/images/call.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 27, 15000, 2, 40, '/images/video.png', NOW(), NOW()),
+
+    -- Expert 28
+    ('TEXT_THERAPY', 28, 15000, 2, 50, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 28, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
+    ('VOICE_THERAPY', 28, 25000, 2, 30, '/images/call.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 28, 15000, 1, 40, '/images/video.png', NOW(), NOW()),
+
+    -- Expert 29
+    ('TEXT_THERAPY', 29, 15000, 1, 50, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 29, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
+    ('VOICE_THERAPY', 29, 25000, 3, 30, '/images/call.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 29, 15000, 1, 40, '/images/video.png', NOW(), NOW()),
+
+    -- Expert 30
+    ('TEXT_THERAPY', 30, 50000, 2, 45, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 30, 70000, 2, 60, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 30, 15000, 4, 70, '/images/chat.png', NOW(), NOW()),
+    ('VOICE_THERAPY', 30, 25000, 2, 30, '/images/call.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 30, 15000, 3, 40, '/images/video.png', NOW(), NOW());
