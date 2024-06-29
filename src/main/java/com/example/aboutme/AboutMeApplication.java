95c0d1d539af5d5567c2e8b1a9638a33cc4c97f9
kjh5848 <kjh5848@gmail.com>
2024-06-29 13:30:16 +0900

    환경변수 설정 완료

--- src/main/java/com/example/aboutme/AboutMeApplication.java
+++ src/main/java/com/example/aboutme/AboutMeApplication.java
@@ -14,6 +14,9 @@ public class AboutMeApplication {
         // Set environment variables
         System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
         System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
+        System.setProperty("OPENAI_API_KEY", dotenv.get("OPENAI_API_KEY"));
+        System.setProperty("OPENAI_ORGANIZATION_ID", dotenv.get("OPENAI_ORGANIZATION_ID"));
+        System.setProperty("OPENAI_PROJECT_ID", dotenv.get("OPENAI_PROJECT_ID"));
 
         SpringApplication.run(AboutMeApplication.class, args);
     }
--- src/main/java/com/example/aboutme/_core/config/OpenAIConfig.java
+++ src/main/java/com/example/aboutme/_core/config/OpenAIConfig.java
@@ -1,19 +1,22 @@
 package com.example.aboutme._core.config;
 
-//@Getter
-//@Configuration
-//@EnableConfigurationProperties
-//@EnableRedisHttpSession
-//public class OpenAIConfig {
-//    @Value("${openai.api.key}")
-//    private String apiKey;
-//
-//    @Value("${openai.api.organization-id}")
-//    private String organizationId;
-//
-//    @Value("${openai.api.project-id}")
-//    private String projectId;
-//
-//    @Value("${openai.api.url}")
-//    private String apiUrl;
-//}
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Getter
+@Configuration
+@EnableRedisHttpSession
+public class OpenAIConfig {
+
+    @Value("${openai.api.key}")
+    private String apiKey;
+
+    @Value("${openai.organization.id}")
+    private String organizationId;
+
+    @Value("${openai.project.id}")
+    private String projectId;
+
+}
