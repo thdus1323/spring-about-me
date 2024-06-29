a0ac89617064bd5306554c8d0541542493f054e8
kjh5848 <kjh5848@gmail.com>
2024-06-30 01:27:23 +0900

    마이페이지 수정완료

--- src/main/java/com/example/aboutme/user/UserRequestRecord/UserProfileUpdateReqDTO.java
+++ src/main/java/com/example/aboutme/user/UserRequestRecord/UserProfileUpdateReqDTO.java
@@ -1,8 +1,8 @@
 package com.example.aboutme.user.UserRequestRecord;
 
 public record UserProfileUpdateReqDTO(
-        String name, // 닉네임
-//        String birthYear,
+        String clientName, // 닉네임
+        String birthYear,
         String gender, // 한글로 받기
         String profileImage
 ) {
