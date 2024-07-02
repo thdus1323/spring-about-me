3cd5eb989dc385ce190763e61b5095a213ea6bb8
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-07-02 17:39:09 +0900

    더미수정

--- src/main/resources/db/voucher_data.sql
+++ src/main/resources/db/voucher_data.sql
@@ -8,16 +8,16 @@ VALUES
 
     -- Expert 22
     ('TEXT_THERAPY', 22, 15000, 1, 50, '/images/chat.png', NOW(), NOW()),
-    ('TEXT_THERAPY', 22, 60000, 4, 50, '/images/chat.png', NOW(), NOW()),
-    ('VOICE_THERAPY', 22, 75000, 3, 30, '/images/call.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 22, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
+    ('VOICE_THERAPY', 22, 20000, 3, 30, '/images/call.png', NOW(), NOW()),
 
     -- Expert 23
-    ('TEXT_THERAPY', 23, 30000, 2, 50, '/images/chat.png', NOW(), NOW()),
-    ('TEXT_THERAPY', 23, 60000, 4, 50, '/images/chat.png', NOW(), NOW()),
-    ('VIDEO_THERAPY', 23, 40000, 2, 40, '/images/video.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 23, 20000, 2, 50, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 23, 20000, 4, 50, '/images/chat.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 23, 30000, 2, 40, '/images/video.png', NOW(), NOW()),
 
     -- Expert 24
-    ('VOICE_THERAPY', 24, 25000, 3, 30, '/images/call.png', NOW(), NOW()),
+    ('VOICE_THERAPY', 24, 15000, 3, 30, '/images/call.png', NOW(), NOW()),
     ('VIDEO_THERAPY', 24, 15000, 4, 40, '/images/video.png', NOW(), NOW()),
     ('VIDEO_THERAPY', 24, 20000, 2, 45, '/images/video.png', NOW(), NOW()),
     ('VIDEO_THERAPY', 24, 30000, 1, 60, '/images/video.png', NOW(), NOW()),
@@ -38,23 +38,23 @@ VALUES
     ('TEXT_THERAPY', 27, 15000, 1, 50, '/images/chat.png', NOW(), NOW()),
     ('TEXT_THERAPY', 27, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
     ('VOICE_THERAPY', 27, 25000, 1, 30, '/images/call.png', NOW(), NOW()),
-    ('VIDEO_THERAPY', 27, 15000, 2, 40, '/images/video.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 27, 30000, 2, 40, '/images/video.png', NOW(), NOW()),
 
     -- Expert 28
     ('TEXT_THERAPY', 28, 15000, 2, 50, '/images/chat.png', NOW(), NOW()),
     ('TEXT_THERAPY', 28, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
     ('VOICE_THERAPY', 28, 25000, 2, 30, '/images/call.png', NOW(), NOW()),
-    ('VIDEO_THERAPY', 28, 15000, 1, 40, '/images/video.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 28, 30000, 1, 40, '/images/video.png', NOW(), NOW()),
 
     -- Expert 29
     ('TEXT_THERAPY', 29, 15000, 1, 50, '/images/chat.png', NOW(), NOW()),
     ('TEXT_THERAPY', 29, 15000, 4, 50, '/images/chat.png', NOW(), NOW()),
     ('VOICE_THERAPY', 29, 25000, 3, 30, '/images/call.png', NOW(), NOW()),
-    ('VIDEO_THERAPY', 29, 15000, 1, 40, '/images/video.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 29, 30000, 1, 40, '/images/video.png', NOW(), NOW()),
 
     -- Expert 30
-    ('TEXT_THERAPY', 30, 50000, 2, 45, '/images/chat.png', NOW(), NOW()),
-    ('TEXT_THERAPY', 30, 70000, 2, 60, '/images/chat.png', NOW(), NOW()),
-    ('TEXT_THERAPY', 30, 15000, 4, 70, '/images/chat.png', NOW(), NOW()),
-    ('VOICE_THERAPY', 30, 25000, 2, 30, '/images/call.png', NOW(), NOW()),
-    ('VIDEO_THERAPY', 30, 15000, 3, 40, '/images/video.png', NOW(), NOW());
+    ('TEXT_THERAPY', 30, 25000, 2, 45, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 30, 25000, 2, 60, '/images/chat.png', NOW(), NOW()),
+    ('TEXT_THERAPY', 30, 25000, 4, 70, '/images/chat.png', NOW(), NOW()),
+    ('VOICE_THERAPY', 30, 30000, 2, 30, '/images/call.png', NOW(), NOW()),
+    ('VIDEO_THERAPY', 30, 35000, 3, 40, '/images/video.png', NOW(), NOW());
